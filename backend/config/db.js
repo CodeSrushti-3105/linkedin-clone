eadff7895ebc63ea72f2672511771e956eb33616
CodeSrushti-3105 <srushtiahire3105@gmail.com>
2025-10-31 18:15:17 +0530

    connected to atlas for hosting

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,16 @@
+// config/db.js
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB connected successfully");
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ MongoDB Connected Successfully");
   } catch (error) {
-    console.error("❌ MongoDB connection failed:", error.message);
-    process.exit(1); // stop app if DB connection fails
+    console.error("❌ MongoDB Connection Error:", error.message);
+    process.exit(1);
   }
 };
 
